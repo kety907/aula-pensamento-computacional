bfef7b6a99fdd888b9ef1f247132cbd96e161bd0
Kethellen <kethellen.campos@escola.pr.gov.br>
2025-06-05 10:37:52 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+// let = faça 
+let campoIdade;
+
+
+function setup() {
+  createCanvas(400, 400);
+  campoIdade = createInput("50");
+}
+
+function draw() {
+  background(220);
+  let idade = campoIdade.value();
+  let recomendacao = geraRecomendacao(idade);
+  text(recomendacao, width / 2, height / 2);
+}
+
+// if = "se"
+// else = " caso contrario"
+function geraRecomendacao(idade) {
+  if(idade >= 10) {
+    if(idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      return "As aventuras de pi";
+    }
+  } else {
+    return "A viagem de chihiro";
+  }
+}
+
